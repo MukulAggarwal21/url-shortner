47fa9d886d8c6c6fcc72105c535ab628b195c464
MUKUL <mukul.2226csit1115@kiet.edu>
2025-02-13 23:37:48 +0530

    nano id and analytics seen

--- routes/url.js
+++ routes/url.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const {handleGenerateNewShortURL} = require('../controllers/url')
+const {handleGenerateNewShortURL , handleGetAnalytics} = require('../controllers/url')
 
 const router = express.Router();
 
 router.post("/" , handleGenerateNewShortURL);
 
-module.exports = {router}; 
+//to show the analytics of clicks 
+router.get('/analytics/:shortId' ,handleGetAnalytics )
+
+module.exports = router; 
