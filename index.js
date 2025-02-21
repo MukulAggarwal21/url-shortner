64987ab676029ddd1c7c6fd87129b6eb01cebc8b
MUKUL <mukul.2226csit1115@kiet.edu>
2025-02-21 08:19:57 +0530

    Front Page UI with Updated EJS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const path = require('path')
 const {connectToMongoDB} = require('./connect')
 const urlRoute = require('./routes/url')
+const staticRoute = require('./routes/staticRouter')
 const URL = require('./models/url')
 const app = express();
 const PORT = 8001;
@@ -9,25 +10,14 @@ const PORT = 8001;
 connectToMongoDB("mongodb://localhost:27017/short-url")
 .then(()=>console.log("MongoDb Connected"));
 
-app.set('view engine', 'ejs');
-app.set('views', path.resolve("./views"))
+app.set('view engine', "ejs");
+app.set('views', path.resolve("./views"));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
 
+app.use('/' , staticRoute)
 
-app.get('/test' , async (req, res)=>{
-  const allUrls = await URL.find({});
-
-  // return res.end(`
-  //   <html>
-  //   <head></head>
-  //   <body>
-  //   <ol>
-  //   ${allUrls.map(url => `<li> ${url.shortId} - ${url.redirectURL}-${url.visitHistory.length}</li>`).join('')}
-  //   </ol>
-  //   </body>
-  //   </html>
-  
 app.use('/url', urlRoute);
 app.get ('/url/:shortId' , async(req, res)=>{
   const shortId = req.params.shortId;
